a946bc6cc72925876b8fb6f0faec0d9b59d0c413
suqun <215144509@qq.com>
2016-04-07 15:45:59 +0800

    delete @JsonView
    delete @JsonView

--- src/main/java/com/larry/model/App.java
+++ src/main/java/com/larry/model/App.java
@@ -13,27 +13,21 @@ import java.util.Date;
 @Entity
 @Data
 public class App {
-    @JsonView(DataTablesOutput.View.class)
     @Id
     @GeneratedValue
     private long id;
     @Column(nullable = false)
-    @JsonView(DataTablesOutput.View.class)
     private String name;
     @Column(nullable = false)
     @URL
-    @JsonView(DataTablesOutput.View.class)
     private String url;
     private String description;
     private String keywords;
     private boolean disabled;
-    @JsonView(DataTablesOutput.View.class)
     private int hot;
 
-    @JsonView(DataTablesOutput.View.class)
     @CreationTimestamp
     private Date createTime;
-    @JsonView(DataTablesOutput.View.class)
     @UpdateTimestamp
     private Date updateTime;
     private boolean isDelete;
