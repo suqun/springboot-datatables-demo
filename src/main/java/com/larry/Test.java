c3306673d4a486644f16b16683691653d63235c0
suqun <215144509@qq.com>
2016-10-19 14:39:43 +0800

    测试

---
+++ src/main/java/com/larry/Test.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2016 Wicresoft, Inc. All rights reserved.
+ */
+package com.larry;
+
+/**
+ * Description of Test
+ *
+ * @author ${User}
+ *         Created on 16/10/19
+ */
+public class Test {
+}
---
+++ src/main/java/com/larry/Test2.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2016 Wicresoft, Inc. All rights reserved.
+ */
+package com.larry;
+
+/**
+ * Description of Test2
+ *
+ * @author larry
+ *         Created on 16/10/19
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+}
